6d1de34c2bc3df52d687e1eb101277ce63b2aa0a
HJY8682 <dogne2@naver.com>
2025-06-02 10:16:06 +0900

    Update 12944_평균 구하기.cpp

--- programmers/12944_평균 구하기.cpp
+++ programmers/12944_평균 구하기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 double solution(vector<int> arr) {
     double answer = 0;              //double 을 사용해 실수로 만들어줌
-    for (int num : arr)             //arr :(에서) num 을 꺼내 대입 반복한다
+    for (int num : arr)             //arr(컨테이너) :(안에있는) num(변수) 을 꺼내 반복한다
     {
         answer += num;              //num을 하나씩 더해서 answer에 대입
     }
